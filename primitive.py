893af10ea0d72f78dcc30f0e7b317b6dee3cf11f
Xingzhong <xu.xeno@gmail.com>
2012-09-27 11:44:29 -0400

    token

---
+++ primitive.py
@@ -0,0 +1,9 @@
+class Token(object):
+    def __init__(self):
+        self.thing = ""
+        self.action = ""
+        self.place = ""
+    
+if  __name__ == '__main__':
+    print "hello Xingzhong"
+    
