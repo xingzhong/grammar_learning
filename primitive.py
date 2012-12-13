5814086b68ff44c13ec0f8d2877ee8d332ed7576
antifeng <ylfeng1986@gmail.com>
2012-12-12 22:20:17 -0500

    change the data structure to graph and modify the merge function

--- primitive.py
+++ primitive.py
@@ -1,95 +1,136 @@
 import pprint 
 import numpy
 import operator
-from parsing import load
+import sys
+import networkx as nx
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
 
 DEBUG = None
 
 class Place(object):
-    def __init__(self, place, thing=set(), action=[]):
+    def __init__(self, pos=(0,0,0, 0), thing=set(), action=[]):
         """
             Construct a Place object.
-                place itself should be represented as a 3/4D vector (x,y,[z],t)
+                pos itself should be represented as a 3/4D vector (x,y,[z],t)
                 thing is an unordered set contains the entities or agents
                 action is an ordered list that abstract the sequence movement, 
                      default is an empty action
         """
         if not action : action = [] 
         self.thing = thing
-        self.place = numpy.array(place)
+        self.pos = pos
         self.action = action
+    def __del__(self):
+        class_name = self.__class__.__name__
+        #print class_name,'self refcount', sys.getrefcount(self), "destroyed"
+
     def __repr__(self):
-        return "@%s do %s at %s"%(self.thing, self.action, self.place)
+        return "@%s do %s at %s"%(self.thing, self.action, self.pos)
         
     def __add__(t1, t2, alpha=[0.5, 0.2, 0.3]):
         """
-            overload "+" for merge two place objects
+            overload "+" for merge two pos objects
         """
-        if t1.place[2] == t2.place[2] and t1.action == t2.action :
+        pos = (0,0,0)
+        if t1.pos[2] == t2.pos[2] and t1.action == t2.action :
             # same time and same action
-            place = (t1.place + t2.place)/2.0
+            pos = ((t1.pos[0] + t2.pos[0])/2.0, (t1.pos[1] + t2.pos[1])/2.0,
+                     (t1.pos[2] + t2.pos[2])/2.0)
+           
             action = t1.action
             thing = t1.thing | t2.thing
-            t3 = Place(place, thing=thing, action=action)
+            t3 = Place(pos, thing=thing, action=action)
             if DEBUG : pprint.pprint ({"t1":t1, "t2":t2, "t3":t3, "type":"thing merge"})
             return t3
             
-        elif len(t1.thing & t2.thing) > 0 and t1.place[2] != t2.place[2]:
+        elif len(t1.thing & t2.thing) > 0 and t1.pos[2] != t2.pos[2]:
             # thing have common 
-            place = max(t1.place, t2.place , key=lambda x: x[2])
+            pos = max(t1.pos, t2.pos , key=lambda x: x[2])
             thing = t1.thing & t2.thing
             action = t1.action.extend(t2.action)
-            t3 = Place(place, thing=thing, action=action)
+            t3 = Place(pos, thing=thing, action=action)
             if DEBUG : pprint.pprint ({"t1":t1, "t2":t2, "t3":t3, "type":"action merge"})
             return t3
             
         return None
     
-    def __sub__(t1, t2):
+    def __sub__(p1, p2):
         """
             overload "-" for find the metric distance 
         """
-        return numpy.linalg.norm(t1.place - t2.place)
+        if p1!=None and p2!=None:
+            return numpy.sqrt((p1.pos[1]-p2.pos[1])**2+(p1.pos[0]-p2.pos[0])**2)
+        else:
+            print 'p1=', p1, 'p2=', p2
+            return 0
             
 
-def merge(places):
-    """
-    for each place, do a possible merge and return new places
-    """
+
+def merge(data):
+    
+    #for each pos, do a possible merge and return G
+    
+    root = data['root']
+    G = data['G']
     candidate = {}
-    itersets = list(places)
-    for p1 in itersets:
-        if p1 not in places :
-            continue
-        candidate[p1] = {}
-        for p2 in places:
-            d = p1 - p2
-            if d:
-                candidate[p1][p2] = d
-        p3 = min(candidate[p1], key= lambda x: candidate[p1].get(x))
-        if p1 - p3 < 20:
-            p4 = p1 + p3
-            places.remove(p1)
-            places.remove(p3)
-            places.add(p4)
+    print G.number_of_nodes()
+    neighbors = G.neighbors(root)
+    
+    for n1 in neighbors:
+        candidate[n1] = {}
+        for n2 in neighbors:
+            #if two places have commom time line, appent them into candidate
+            if n1.pos[2] < n2.pos[3] or n2.pos[2] < n1.pos[3]:
+                candidate[n1][n2] = n1 - n2
+
+            #get the min candidate to merge
+            n3 = min(candidate[n1])
+            if n1 - n3 < 10 and n1!=n3:
+                    n4 = n1 + n3
+                    G.add_node(n4)
+                    G.add_edge(n4, G.succ(n1))
+                    G.add_edge(n1, n4)
+                    G.remove_node(n1)
+                    G.remove_node(n3)
+    return G
+
+def load_create(file):
+    soup = BeautifulSoup(open(file))
+    G=nx.DiGraph()
+
+    root = Place(pos = (0, 0, 0, 0), thing = set())
+    G.add_node(root)
                 
-    return places
-    
-if  __name__ == '__main__':
-    print "hello Xingzhong"
-    data = load('dataset/1-11200.xgtf')
-
-    places = set()
-    for d in data[:50] :
-        for t in eval("range(%s+1)"%d[0]['framespan'].replace(":",",")):
+    for obj in soup.find_all('object'):
+        idnum = obj['id']
+        leftneighbor = root
+        for d in obj.find_all('data:bbox'):
             p = Place(
-                place = ( int(d[0]['x']), int(d[0]['y']), t ),
-                thing = set([int(d[1])]), 
+                pos = ( int(d['x']), int(d['y']), int(d['framespan'].split(':')[0]), int(d['framespan'].split(':')[1])),
+                thing = set([])
                 )
-            places.add( p ) 
-            
-    places = merge(places)
-    pprint.pprint (places) 
+            #print p
+            if leftneighbor-p > 2 or leftneighbor == root:
+                G.add_node(p)
+                G.add_edge(leftneighbor, p)
+                leftneighbor = p
+    return {'root':root, 'G':G }
 
+if  __name__ == '__main__':
 
+    data = load_create('dataset/1-11200.xgtf')
+    G = merge(data)
     
+    # draw graph
+    pos = {}
+    for n in G.nodes():
+        pos[n] = n.pos[:2]
+    nx.draw(G,node_color='r', with_labels=False,cmap=plt.cm.Greys,pos=pos,vmin=0,vmax=1)
+    
+    # show graph
+    plt.show()
+
+
+
