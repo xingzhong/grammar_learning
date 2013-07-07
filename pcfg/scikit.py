5b61ee668d27c09899a35a083e484c99b45ef781
Xingzhong <xu.xeno@gmail.com>
2013-07-07 13:32:39 -0400

    update the example

--- pcfg/scikit.py
+++ pcfg/scikit.py
@@ -3,36 +3,47 @@ import numpy as np
 
 def grammar ():
 	r2 = pcfg.Rule()
-	A = pcfg.NT('A')
-	B = pcfg.NT('B')
-	C = pcfg.NT('C')
-	a = pcfg.T('a', [0.0, 0.0], 0.4*np.identity(2))
-	b = pcfg.T('b', [1.0, 1.0], 0.4*np.identity(2))
-	c = pcfg.T('c', [-1.0, -1.0], 0.4*np.identity(2))
+	UP = pcfg.NT('UP')
+	ST = pcfg.NT('ST')
+	DO = pcfg.NT('DO')
+	u = pcfg.T('u', [1.0, 1.0, 1.0], 0.4*np.identity(3))
+	s = pcfg.T('s', [0.0, 0.0, 0.0], 0.4*np.identity(3))
+	d = pcfg.T('d', [-1.0, -1.0, -1.0], 0.4*np.identity(3))
 	empty = pcfg.Empty(None)
-	r2[(A, a, A, a)] = 0.45
-	r2[(A, a, B, a)] = 0.45
-	r2[(A, empty)] = 0.1
-	r2[(B, b, A)] = 0.33
-	r2[(B, b, B)] = 0.33
-	r2[(B, b, C)] = 0.33
-	r2[(B, empty)] = 0.01
-	r2[(C, B, c)] = 0.4
-	r2[(C, C, c)] = 0.4
-	r2[(C, empty)] = 0.2
+	r2[(UP, u, UP, u)] = 0.45
+	r2[(UP, u, ST, u)] = 0.45
+	r2[(UP, empty)] = 0.1
+	r2[(ST, s, UP)] = 0.33
+	r2[(ST, s, ST)] = 0.33
+	r2[(ST, s, DO)] = 0.33
+	r2[(ST, empty)] = 0.01
+	r2[(DO, ST, d)] = 0.4
+	r2[(DO, DO, d)] = 0.4
+	r2[(DO, empty)] = 0.2
 	return r2
 
 def data ():
-	seq = np.array([[0.0,0.0], [1.0,1.0], [1.0,1.0], [-1.0, -1.0], [0.0, 0.0]])
+	seq = np.array([
+		[1.0,1.0,1.0], 
+		[1.0,1.0,1.0], 
+		[0.0,0.0,0.0],  
+		[1.0,1.0,1.0],
+		[-1.0, -1.0, -1.0],
+		[1.0,1.0,1.0],
+		])
+	noise = 0.2*np.random.randn(*seq.shape)
+	seq = seq + noise
 	return seq
 
 g = grammar()
 s = data ()
 model = pcfg.BasePCFG(grammar=g)
 
+
 print g
 print s
 lik, t = model.decode(s)
+print model
 
 print t
 
