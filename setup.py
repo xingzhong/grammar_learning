3ab6e05a6850273953b50df1496f35422d12d81e
Xingzhong <xu.xeno@gmail.com>
2014-12-09 08:58:36 -0500

    restructural

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qcyk',
+      version='0.1',
+      description='quick cyk parsing',
+      url='https://github.com/xingzhong/grammar_learning',
+      author='Xingzhong Xu',
+      author_email='xu@sirxu.com',
+      license='MIT',
+      packages=['parsing', 'research'],
+      zip_safe=False)
